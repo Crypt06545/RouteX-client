39c72878e01252dd3f17b1126de8f9a15518120c
Crypt06545 <mehedihasansagor2005@gmail.com>
2024-12-07 13:17:54 +0600

    add animation on error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import Lottie from "lottie-react";
+import errorAnimation from "../assets/error.json";
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default ErrorPage
+  // Function to navigate back to the home page
+  const handleBackToHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center bg-gray-400 min-h-screen">
+      <div className="w-1/3 h-auto">
+        <Lottie animationData={errorAnimation} />
+      </div>
+      <button
+        onClick={handleBackToHome}
+        className="mt-6 px-6 py-3 bg-green-600 text-white rounded-full hover:bg-green-700" 
+      >
+        Back to Home
+      </button>
+    </div>
+  );
+};
+
+export default ErrorPage;
